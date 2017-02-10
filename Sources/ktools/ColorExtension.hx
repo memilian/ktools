d9a442a7ede05ad77a56781213c44992144a2846
memilian <mathieu.emilian@viacesi.fr>
2017-02-10 11:07:51 +0100

    added dependencies to haxelib.json + added ColorExtension

---
+++ Sources/ktools/ColorExtension.hx
@@ -0,0 +1,12 @@
+package ktools;
+
+import kha.Color;
+import kha.math.FastVector4;
+
+class ColorExtension{
+
+	inline public static function toVec4(col : Color){
+		return new FastVector4(col.R, col.G, col.B, col.A);
+	}
+
+}
