d1e27dd56bff1e183be8eeb09245eecf4ebdd9a1
memilian <mathieu.emilian@viacesi.fr>
2017-03-02 11:42:53 +0100

    added radian/degree conversions

--- Sources/ktools/FloatExtension.hx
+++ Sources/ktools/FloatExtension.hx
@@ -4,6 +4,9 @@ import kha.FastFloat;
 
 class FloatExtension{
 
+	static var rad2deg = 57.2957795131;
+	static var deg2rad = 0.0174532925199;
+
 	public static function clamp(val : Float, min : Float, max : Float){
 		return val > max ? max : val < min ? min : val;
 	}
@@ -15,6 +18,14 @@ class FloatExtension{
 	public static inline function precision(f : Float, precision : Int){
 		var r = Math.pow(10, precision);
 		return Math.fround(f * r) / r;
+	}
+
+	public static inline function toDegree(f : Float){
+		return f * rad2deg;
+	}
+
+	public static inline function toRadian(f : Float){
+		return f * deg2rad;
 	}
 }
 
